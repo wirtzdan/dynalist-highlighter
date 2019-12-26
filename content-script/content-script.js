6a4fb9d7d7d249ab6a049f3338baf11596ef01b8
Daniel Wirtz <danielwirtzx@gmail.com>
2019-12-26 22:14:08 +0100

    :truck: Renaming to tooltip instead of highlighter

--- content-script/content-script.js
+++ content-script/content-script.js
@@ -2,17 +2,17 @@ console.log("Dynalist Annotator Loaded");
 import "../src/styles/style.css";
 import MarkerSVG from "../src/assets/marker.svg";
 
-const Highlighter = document.createElement("div");
-Highlighter.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.37 16.51"><path d="M12.87 16.51a.5.5 0 01-.5-.5v-2.26L9.84 11.2a1 1 0 01-.3-.73V8.79L4 8.76v1.57a1 1 0 01-.3.73L1 13.73V16a.5.5 0 01-.5.5.5.5 0 01-.5-.5v-2.25A1 1 0 01.3 13L3 10.35V8.79a1 1 0 011-1h5.5a1 1 0 011 1v1.68L13.07 13a1 1 0 01.3.73V16a.5.5 0 01-.5.51z" fill="#4e4d4d"/><path d="M10 6H3.48V3a.46.46 0 01.28-.43L9.2.05a.6.6 0 01.8.54z" fill="#3f7dc0"/></svg>`;
-Highlighter.classList.add("dyn-highlighter");
-Highlighter.addEventListener("mousedown", handleClick)
-document.body.appendChild(Highlighter);
+const Tooltip = document.createElement("div");
+Tooltip.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.37 16.51"><path d="M12.87 16.51a.5.5 0 01-.5-.5v-2.26L9.84 11.2a1 1 0 01-.3-.73V8.79L4 8.76v1.57a1 1 0 01-.3.73L1 13.73V16a.5.5 0 01-.5.5.5.5 0 01-.5-.5v-2.25A1 1 0 01.3 13L3 10.35V8.79a1 1 0 011-1h5.5a1 1 0 011 1v1.68L13.07 13a1 1 0 01.3.73V16a.5.5 0 01-.5.51z" fill="#4e4d4d"/><path d="M10 6H3.48V3a.46.46 0 01.28-.43L9.2.05a.6.6 0 01.8.54z" fill="#3f7dc0"/></svg>`;
+Tooltip.classList.add("dyn-tooltip");
+Tooltip.addEventListener("mousedown", handleClick)
+document.body.appendChild(Tooltip;
 
-const SendButton = document.createElement("button");
-SendButton.innerHTML = `Send to Dynalist`;
-SendButton.classList.add("dyn-send-button");
-SendButton.addEventListener("click", SendHighlights)
-document.body.appendChild(SendButton);
+const SubmitButton = document.createElement("button");
+SubmitButton.innerHTML = `Send to Dynalist`;
+SubmitButton.classList.add("dyn-send-button");
+SubmitButton.addEventListener("click", SendHighlights)
+document.body.appendChild(SubmitButton);
 
 function createHighlightElement() {
   const span = document.createElement("span");
@@ -36,7 +36,7 @@ function handleClick(e) {
 }
 
 function showHighlighter({ mouseX, mouseY }) {
-  const element = document.getElementsByClassName("dyn-highlighter");
+  const element = document.getElementsByClassName("dyn-tooltip");
   element[0].setAttribute(
     "style",
     `display:block; top: ${mouseY + 5}px; left: ${mouseX + 5}px;`
@@ -44,7 +44,7 @@ function showHighlighter({ mouseX, mouseY }) {
 }
 
 function hideHighlighter() {
-  const element = document.getElementsByClassName("dyn-highlighter");
+  const element = document.getElementsByClassName("dyn-tooltip");
     element[0].setAttribute("style",`display:none;`);
 }
 
