cfe97b149df144b90104d9131b173a215eebb95c
Daniel Wirtz <danielwirtzx@gmail.com>
2019-12-27 21:06:00 +0100

    :sparkles: Inject script on icon click

--- background/background.js
+++ background/background.js
@@ -1,1 +1,6 @@
-console.log('I am from background script!!');
+console.log("I am from background script!!");
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.insertCSS({ file: "./static/css/content-script.css" });
+  chrome.tabs.executeScript(null, { file: "content-script.js" });
+});
