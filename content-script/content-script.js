7663d9234991009973b19e6152695b80cc98181c
Daniel Wirtz <danielwirtzx@gmail.com>
2020-01-19 11:00:37 +0100

    :sparkles: Add widget

--- content-script/content-script.js
+++ content-script/content-script.js
@@ -29,36 +29,45 @@ const dynDocument = axios.create({
   timeout: 10000
 });
 
-const title = document.title;
 const url = document.URL;
 
 const Adder = createAdder();
 const Tooltip = createTooltip();
-const SubmitButton = createSubmitButton();
+const Widget = createWidget();
 
 // ---- Create all elements ---- //
 // TODO: Try to import the SVGs from the Filesystem
 function createAdder() {
   const Adder = document.createElement("div");
-  Adder.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.37 16.51"><path d="M12.87 16.51a.5.5 0 01-.5-.5v-2.26L9.84 11.2a1 1 0 01-.3-.73V8.79L4 8.76v1.57a1 1 0 01-.3.73L1 13.73V16a.5.5 0 01-.5.5.5.5 0 01-.5-.5v-2.25A1 1 0 01.3 13L3 10.35V8.79a1 1 0 011-1h5.5a1 1 0 011 1v1.68L13.07 13a1 1 0 01.3.73V16a.5.5 0 01-.5.51z" fill="#4e4d4d"/><path d="M10 6H3.48V3a.46.46 0 01.28-.43L9.2.05a.6.6 0 01.8.54z" fill="#FFD43B"/></svg>`;
+  Adder.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"viewBox="0 0 13.37 16.51"><path d="M12.87 16.51a.5.5 0 01-.5-.5v-2.26L9.84 11.2a1 1 0 01-.3-.73V8.79L4 8.76v1.57a1 1 0 01-.3.73L1 13.73V16a.5.5 0 01-.5.5.5.5 0 01-.5-.5v-2.25A1 1 0 01.3 13L3 10.35V8.79a1 1 0 011-1h5.5a1 1 0 011 1v1.68L13.07 13a1 1 0 01.3.73V16a.5.5 0 01-.5.51z"fill="#4e4d4d"/><path d="M10 6H3.48V3a.46.46 0 01.28-.43L9.2.05a.6.6 0 01.8.54z"fill="#FFD43B"/></svg>`;
   Adder.classList.add("dyn-adder");
   return Adder;
 }
 
-function createSubmitButton() {
-  const SubmitButton = document.createElement("div");
-  SubmitButton.innerHTML = `Send to Dynalist`;
-  SubmitButton.classList.add("dyn-submit-button");
-  return SubmitButton;
-}
-
 function createTooltip() {
   const Tooltip = document.createElement("div");
-  Tooltip.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-trash-2" viewBox="0 0 24 24">
-  <path d="M3 6h18M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2M10 11v6M14 11v6"/>
-</svg>`;
+  Tooltip.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"fill="none"stroke="currentColor"stroke-linecap="round"stroke-linejoin="round"stroke-width="2"class="feather feather-trash-2"viewBox="0 0 24 24"><path d="M3 6h18M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2M10 11v6M14 11v6"/></svg>`;
   Tooltip.classList.add("dyn-tooltip");
   return Tooltip;
+}
+
+function createWidget() {
+  const Widget = document.createElement("div");
+  Widget.innerHTML = `
+<textarea class="dyn-title" name="title">
+${document.title || document.location.href.split("/").pop()}
+</textarea>
+<div class="dyn-widget-bar">
+  <button class="dyn-button">Send to Dynalist</button>
+  <div class="dyn-highlight-counter">
+    <span class="dyn-highlight-number">0</span>
+    <div>Highlights</div>
+  </div>
+</div>
+`;
+
+  Widget.classList.add("dyn-widget");
+  return Widget;
 }
 
 // ---- Functions ---- //
@@ -75,33 +84,51 @@ function getMousePosition(e) {
       e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
   }
 
-  return { mouseX, mouseY };
+  return {
+    mouseX,
+    mouseY
+  };
 }
 
 function addHighlight(e) {
   highlighter.highlightSelection("dyn-highlight");
   highlighter.getHighlightForElement(e.target);
-  const selectionTxt = rangy.getSelection();
   rangy.getSelection().removeAllRanges();
-  const hl = highlighter;
+
+  document.getElementsByClassName("dyn-highlight-number")[0].innerHTML =
+    highlighter.highlights.length;
+
+  if (highlighter.highlights.length > 0) {
+    document.getElementsByClassName("dyn-highlight-counter")[0].style.display =
+      "flex";
+  }
 }
 
 function removeHighlight(target) {
   var highlight = highlighter.getHighlightForElement(target);
+
   if (highlight) {
     highlighter.removeHighlights([highlight]);
+    document.getElementsByClassName("dyn-highlight-number")[0].innerHTML =
+      highlighter.highlights.length;
+    if (highlighter.highlights.length === 0) {
+      document.getElementsByClassName(
+        "dyn-highlight-counter"
+      )[0].style.display = "none";
+    }
   }
 }
 
 function showElement(el, e) {
   if (e) {
     const { mouseX, mouseY } = getMousePosition(e);
+
     el.setAttribute(
       "style",
       `display: block; top: ${mouseY}px; left: ${mouseX}px;`
     );
   } else {
-    el.setAttribute("style", `display: block;`);
+    el.setAttribute("style", `display: block; `);
   }
 }
 
@@ -110,6 +137,48 @@ function hideElement(el) {
 }
 
 async function sendHighlightsToDynalist(key, fileid) {
+  const title = document.getElementsByClassName("dyn-title")[0].value.trim();
+
+  document.getElementsByClassName(
+    "dyn-widget"
+  )[0].innerHTML = `<div class="dyn-widget-container"><!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL -->
+<svg width="44" height="44" viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg" stroke="#038aff">
+    <g fill="none" fill-rule="evenodd" stroke-width="2">
+        <circle cx="22" cy="22" r="1">
+            <animate attributeName="r"
+                begin="0s" dur="1.8s"
+                values="1; 20"
+                calcMode="spline"
+                keyTimes="0; 1"
+                keySplines="0.165, 0.84, 0.44, 1"
+                repeatCount="indefinite" />
+            <animate attributeName="stroke-opacity"
+                begin="0s" dur="1.8s"
+                values="1; 0"
+                calcMode="spline"
+                keyTimes="0; 1"
+                keySplines="0.3, 0.61, 0.355, 1"
+                repeatCount="indefinite" />
+        </circle>
+        <circle cx="22" cy="22" r="1">
+            <animate attributeName="r"
+                begin="-0.9s" dur="1.8s"
+                values="1; 20"
+                calcMode="spline"
+                keyTimes="0; 1"
+                keySplines="0.165, 0.84, 0.44, 1"
+                repeatCount="indefinite" />
+            <animate attributeName="stroke-opacity"
+                begin="-0.9s" dur="1.8s"
+                values="1; 0"
+                calcMode="spline"
+                keyTimes="0; 1"
+                keySplines="0.3, 0.61, 0.355, 1"
+                repeatCount="indefinite" />
+        </circle>
+    </g>
+</svg></div>`;
+
   const turndown = new TurndownService();
 
   const response = await dynInbox.post("/add", {
@@ -117,8 +186,8 @@ async function sendHighlightsToDynalist(key, fileid) {
     index: "0",
     content: `[${title}](${url})`
   });
+  console.log("TCL: sendHighlightsToDynalist -> response", response);
 
-  // TODO: Implement Dropdown in the options where you choose a file
   for (const highlight of highlighter.highlights) {
     await dynDocument.post("/edit", {
       token: key,
@@ -133,6 +202,10 @@ async function sendHighlightsToDynalist(key, fileid) {
       ]
     });
   }
+
+  document.getElementsByClassName(
+    "dyn-widget"
+  )[0].innerHTML = `<div class="dyn-widget-container"><p>Bookmark added to Dynalist:</p><a href="${"#"}">${title}</a></div>`;
 }
 
 // ---- Handle Functions ---- //
@@ -166,13 +239,15 @@ function handleMousedown(e) {
 }
 
 function handleClick() {
-  // TODO: Extract all the text nodes from the highlight object
+  console.log("Clicked!");
 
   chrome.storage.sync.get(["key", "fileid"], function(result) {
     const { key, fileid } = result;
 
     if (!key || !fileid) {
-      alert("API key and File ID are missing.");
+      alert(
+        "You need to add your API key to make the connection to Dynalist. Head to the options (Right click on the extension icon) to add the key."
+      );
       sendHighlightsToDynalist(key, fileid);
     } else {
       sendHighlightsToDynalist(key, fileid);
@@ -189,7 +264,9 @@ function addAllEventListener() {
 
   Adder.addEventListener("mousedown", e => addHighlight(e));
 
-  SubmitButton.addEventListener("click", () => handleClick());
+  document
+    .getElementsByClassName("dyn-button")[0]
+    .addEventListener("click", () => handleClick());
 }
 
 function removeAllEventListener() {
@@ -199,7 +276,9 @@ function removeAllEventListener() {
 
   Adder.removeEventListener("mousedown", e => addHighlight(e));
 
-  SubmitButton.removeEventListener("click", () => handleClick());
+  document
+    .getElementsByClassName("dyn-button")[0]
+    .removeEventListener("click", () => handleClick());
 }
 
 // ---- On Message Listener ---- //
@@ -215,18 +294,20 @@ let active = false;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("TCL: request", request);
+
   if (request.msg === "deactivate") {
     sendResponse("Response: Deactivated");
     highlighter.removeAllHighlights();
     removeAllEventListener();
-    removeElements(["dyn-adder", "dyn-submit-button", "dyn-tooltip"]);
+    removeElements(["dyn-adder", "dyn-widget", "dyn-tooltip"]);
   } else if (request.msg === "activate" && active === false) {
     if (document.getElementsByClassName("dyn-adder").length > 0) {
       return;
     }
+
     sendResponse("Response: Activated");
+    document.body.appendChild(Widget);
     document.body.appendChild(Adder);
-    document.body.appendChild(SubmitButton);
     document.body.appendChild(Tooltip);
     addAllEventListener();
     active = true;
